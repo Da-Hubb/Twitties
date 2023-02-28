edea954256ae09f4cc06acb06209b902ae9d2c58
Da-Hubb <josemariaofurum@gmail.com>
2023-03-01 00:05:16 +0100

    added different profile pictures, scripted more functionalities and also added some svg icons to support the project

--- script.js
+++ script.js
@@ -1,19 +1,147 @@
-// pages
-const boxes = document.querySelectorAll(".box");
+// Array of profile picture of contacts
+
+const contact = [
+    {
+        dp: "assets/profile/profile1.jpg",
+        naming: "Josh",
+        quote: "i can be anything"
+    },
+    {
+        dp: "assets/profile/profile2.jpg",
+        naming: "Lollipop",
+        quote: "Come for the king?..."
+    },
+    {
+        dp: "assets/profile/profile3.jpg",
+        naming: "Mary Jane",
+        quote: "Eat me as you like, i love it"
+    },
+    {
+        dp: "assets/profile/profile4.jpg",
+        naming: "Ericsson",
+        quote: "I actually don't give a fuck"
+    },
+    {
+        dp: "assets/profile/profile5.jfif",
+        naming: "Squid Master",
+        quote: "Game of squid"
+    },
+    {
+        dp: "assets/profile/profile6.jpg",
+        naming: "Hacker",
+        quote: "Infiltration"
+    },
+    {
+        dp: "assets/profile/profile7.jfif",
+        naming: "Edward Higginson",
+        quote: "Inner peace"
+    },
+    {
+        dp: "assets/profile/profile8.jpg",
+        naming: "My guy",
+        quote: "Nerd will rule one day"
+    },
+    {
+        dp: "assets/profile/profile9.jfif",
+        naming: "Deborah",
+        quote: "The happier the merrier"
+    },
+    {
+        dp: "assets/profile/profile10.jfif",
+        naming: "Isabella",
+        quote: "The world is so big"
+    },
+    {
+        dp: "assets/profile/profile11.jfif",
+        naming: "Dragon Lord",
+        quote: ""
+    },
+    {
+        dp: "assets/profile/profile12.jpg",
+        naming: "Jessica",
+        quote: "Lick you balls"
+    },
+    {
+        dp: "assets/profile/profile13.webp",
+        naming: "Mirabel",
+        quote: "Call when you need me"
+    },
+    {
+        dp: "assets/profile/profile14.png",
+        naming: "Nate",
+        quote: "Meticulously active"
+    },
+    {
+        dp: "assets/profile/profile15.jfif",
+        naming: "Car Dealer",
+        quote: "I sell the ride you need"
+    },
+]
 
 
-// the current page 
-const pageon = document.querySelectorAll(".page");
 
-// hero_header and current page
-const hero_header = document.querySelector(".hero_header");
+/* elements needed */
+const pages = document.querySelectorAll(".box");// diferent pages
 
-const currentpage = document.querySelector(".currentpage");
+const switchpage = document.querySelectorAll(".page");// the indicator for the current page i am on
 
-const messages = document.querySelector(".messages");
+const hero_header = document.querySelector(".hero_header");// the toppest header
 
-// to change the active page we are on
-pageon.forEach(activepage => {
+const sub_header = document.querySelector(".currentpage");// the subordinate header
+
+const messages = document.querySelector(".messages");// where the dm i am dmming is
+
+const openContact = document.querySelector(".addNewChat"); // the openchat fixed at the bottom
+
+const closeContact = document.getElementById("return_btn");
+
+const contactList = document.querySelector(".contactList");// the contact list
+
+const contactListDisplay = document.querySelector(".contactList_display");// availableContacts
+
+const totalContact = document.querySelector(".num_of_contact");
+
+
+const showAvailableContact = () => {
+    
+    let randomNum = Math.floor(Math.random() * 200);
+    
+    let contactNum = contact.length;
+    
+
+    for(x = 0; x < randomNum; x++) {
+        let ranConNum = Math.floor(Math.random() * contactNum);
+        
+        totalContact.textContent = x;
+
+        contactListDisplay.innerHTML += `
+        <div class="indi">
+            <picture class="indi_img">
+                <img src=${contact[ranConNum].dp} alt="people" loading="lazy" class="indi_pic">
+            </picture>
+    
+            <span class="indi_name">
+                <h5>${contact[ranConNum].naming}</h5>
+                <p class="quote">${contact[ranConNum].quote}</p>
+            </span>
+        </div>`
+    }
+}// show available contact
+
+closeContact.addEventListener("click", () => {
+    contactList.classList.remove("showtrans");
+})// close contact list
+
+openContact.addEventListener("click", () => {
+    contactList.classList.add("showtrans");
+})// show contact list
+
+window.addEventListener("DOMContentLoaded", () => {
+    changePage(-100);
+    showAvailableContact();
+})// window on dom content loaded
+
+switchpage.forEach(activepage => {
     activepage.addEventListener("click", (e) => {
         let data = e.currentTarget.dataset.id;
 
@@ -27,47 +155,36 @@ pageon.forEach(activepage => {
 
         else changePage(-400);
         
-        pageon.forEach(currentpages => {
+        switchpage.forEach(currentpages => {
             currentpages.classList.remove("pageactive");
         })
         
         e.currentTarget.classList.add("pageactive");
     })
-})
+})// to swicthpage
 
-// window on dom content loaded
-window.addEventListener("DOMContentLoaded", () => {
-    changePage(-100);
-})
-
-// window on scroll
-boxes.forEach(page => {
+pages.forEach(page => {
     page.addEventListener("scroll", (e) => {
         scrooling(e);
     })
-})
+})// any of the pages onscrool make subheader fixed
 
 messages.addEventListener("scroll", (e) => {
     scrooling(e);
-})
+})// message on scroll should make subheader fixed
 
-
-/*********** All functions be found here ****** */
-
-// function scroll
 const scrooling = (e) => {
     let heroHeight = hero_header.getBoundingClientRect().height;
     
     let PageY = e.currentTarget.scrollTop;
     
-    PageY > heroHeight ? currentpage.classList.add("glued") : currentpage.classList.remove("glued");
+    PageY > heroHeight ? sub_header.classList.add("glued") : sub_header.classList.remove("glued");
     
-}
+}// function scroll
 
-// fuction to change page 
 const changePage = (num) => {
-    boxes.forEach(boxPage => {
+    pages.forEach(boxPage => {
         boxPage.style.transform = `translateX(${num}%)`;
         num += 100;
     })
-}
+}// fuction to change page 
